2d307881309bd598b443c59b7fde0b8cbe01aefa
Fugazzii <sichinavailia@gmail.com>
2024-02-13 20:33:35 +0400

    Set up rpc

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -1,5 +1,9 @@
+import { JsonRpcClient } from "../../json-rpc/lib/rpc-client";
+import { JsonRpcServer } from "../../json-rpc/lib/rpc-server";
+import { TcpClient, TcpServer } from "../../json-rpc/lib/transport";
 import { EventStore } from "./event-store";
 import { MessageEvent } from "./message-event";
+import chalk from 'chalk';
 
 enum NodeState {
     Candidate,
@@ -10,13 +14,12 @@ enum NodeState {
 export class Node {
 
     private _port!: number;
-    private readonly rpcServer: any;
-    private readonly rpcClient: any;
-    private readonly _nodeAddresses: Array<string>;
-
-    private readonly _ledger: EventStore<MessageEvent>;
-
     private _currentState: NodeState;
+
+    private rpcServer!: JsonRpcServer;
+    private rpcClient!: JsonRpcClient;
+    private readonly _nodeAddresses: Array<string>;
+    private readonly _ledger: EventStore<MessageEvent>;
 
     public constructor() {
         this._nodeAddresses = [];
@@ -25,26 +28,49 @@ export class Node {
     }
 
     public start(port: number) {
-        console.log("Starting node on", port);
+        console.log(chalk.green(`Starting node on ${port}`));
         this._port = port;
+        const hostOptions = { 
+            hostname: "localhost",
+            port
+        };
+        this.rpcServer = JsonRpcServer.run({
+            transport: TcpServer.run(hostOptions)
+        });
+        this.rpcClient = JsonRpcClient.connect({
+            transport: TcpClient.connect(hostOptions)
+        });
     }
+
     public kill() {
-        console.log("Killing node on ", this._port);
-        process.exit(1);
+        console.log(chalk.red(`Killing node on ${this._port}`));
+        this.rpcClient.close();
+        this.rpcServer.close();
     }
 
     public publish(message: MessageEvent) {}
 
     public displayLedger(message: string, limit = 0, offset = 0) {
         const result = this._ledger.findMany({ limit, offset });
-        console.log(message, result);
+        console.log(chalk.blue(`${message}:`), result);
     }
-    
-    public becomeLeader() {}
-    public becomeFollower() {}
-    public becomeCandidate() {}
+
+    public becomeLeader() {
+        this._currentState = NodeState.Leader;
+        console.log(chalk.yellow("Becoming Leader"));
+    }
+
+    public becomeFollower() {
+        this._currentState = NodeState.Follower;
+        console.log(chalk.yellow("Becoming Follower"));
+    }
+
+    public becomeCandidate() {
+        this._currentState = NodeState.Candidate;
+        console.log(chalk.yellow("Becoming Candidate"));
+    }
 
     public sendHeartbeats() {
-        
+
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -9,27 +9,32 @@ node1.start(3000);
 node2.start(3001);
 node3.start(3002);
 
-node1.publish(MessageEvent.new("Hello 1"));
-displayAll();
+setTimeout(() => {
+    runSimulation();  
+}, 1000);
 
-node2.publish(MessageEvent.new("Hello 2"));
-displayAll();
+function runSimulation() {
+    node1.publish(MessageEvent.new("Hello 1"));
+    displayAll();
 
-/** Simulate failure */
-node3.kill();
-node2.publish(MessageEvent.new("Hello 3"));
-displayAll();
+    node2.publish(MessageEvent.new("Hello 2"));
+    displayAll();
 
-/** Check if node3 is updated with correct order */
-node3.start(3002);
-node1.publish(MessageEvent.new("Hello 4"));
-displayAll();
+    /** Simulate failure */
+    node3.kill();
+    node2.publish(MessageEvent.new("Hello 3"));
+    displayAll();
 
-node2.publish(MessageEvent.new("Hello 5"));
+    /** Check if node3 is updated with correct order */
+    node3.start(3002);
+    node1.publish(MessageEvent.new("Hello 4"));
+    displayAll();
 
+    node2.publish(MessageEvent.new("Hello 5"));
 
-function displayAll() {
-    node1.displayLedger("Node 1 Ledger");
-    node2.displayLedger("Node 2 Ledger");
-    node3.displayLedger("Node 3 Ledger");    
+    function displayAll() {
+        node1.displayLedger("Node 1 Ledger");
+        node2.displayLedger("Node 2 Ledger");
+        node3.displayLedger("Node 3 Ledger");    
+    }
 }
